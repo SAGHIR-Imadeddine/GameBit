9e5ed88469b42594f46023dd09b592449ed8a165
ATalemsi <mohamadtalemsi@gmail.com>
2023-11-03 14:39:40 +0100

    js

--- abdo.js
+++ abdo.js
@@ -1,28 +1,38 @@
-//   //slides and buttons left and right
-//   const rightBtn = document.getElementById('right');
-//   const leftBtn = document.getElementById('left')
-//   const slides = document.querySelectorAll(".slide");
-//   let currentSlide = 0;
-  
-//   function displaySlides(){
-//     slides.forEach(slide =>{
-//         slide.style.display = "none"
-//     })
-  
-//     slides[currentSlide].style.display = "block";
-//   }
-  
-//   displaySlides();
-  
-//   rightBtn.addEventListener("click", () => {
-//     currentSlide = (currentSlide + 1) % slides.length;
-//     displaySlides();
-//   });
-  
-//   leftBtn.addEventListener("click", () => {
-//     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-//     displaySlides();
-//   });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navigationLinks = document.getElementById("navbar-sticky");
+    
+    menuToggle.addEventListener("click", function () {
+        navigationLinks.classList.toggle("hidden"); 
+    });
+});
+// *********************************************************************************
+const cartToggle = document.getElementById("cart-toggle");
+const cartSidebar = document.getElementById("cart-sidebar");
+const closeCart = document.getElementById("close-cart");
+
+cartToggle.addEventListener("click", function (e) {
+    e.stopPropagation(); // Prevent the click event from reaching the document
+
+    // Toggle the cart sidebar
+    cartSidebar.classList.remove("translate-x-full");
+});
+
+document.addEventListener("click", function (e) {
+    // Close the cart sidebar when clicking outside of it
+    if (!cartSidebar.contains(e.target) && e.target !== cartToggle) {
+        cartSidebar.classList.add("translate-x-full");
+    }
+});
+
+closeCart.addEventListener("click", function (e) {
+    e.stopPropagation(); // Prevent the click event from reaching the document
+
+    // Close the cart sidebar
+    cartSidebar.classList.add("translate-x-full");
+});
+//-----------------------------------------------------------------------
 const rightBtn = document.getElementById('right');
     const leftBtn = document.getElementById('left');
     const slides = document.querySelectorAll(".slide");
@@ -67,3 +77,6 @@ const rightBtn = document.getElementById('right');
         prevSlide();
         autoSlideInterval = setInterval(nextSlide, 3000);
     });
+
+
+   
